803c59840667b22219dd1fcd8f10861554aa9858
kramerij <kramerisabelle09@gmail.com>
2026-02-16 11:40:45 -0500

    Added account controllers for logins and account creation

---
+++ Code/FindMyFlickWebsite/FindMyFlickWebsite.Server/Controllers/AccountController.cs
@@ -0,0 +1,106 @@
+﻿using FindMyFlickWebsite.Server.DataModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt; 
+using System.Security.Claims;
+
+namespace FindMyFlickWebsite.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        private IConfiguration _configuration;
+
+        public AccountController(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+
+        }
+        //register a new user
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] Register model)
+        {
+            var userExists = await _userManager.FindByNameAsync(model.Username);
+            if (userExists != null)
+                return Conflict("User already exists!");
+            IdentityUser user = new()
+            {
+                Email = model.Email,
+                UserName = model.Username
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+                return Ok("User created successfully!");
+            return BadRequest(result.Errors);
+        }
+
+        //login a user
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] Login model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var userRoles = await _userManager.GetRolesAsync(user);
+                var authClaims = new List<Claim>
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.UserName!),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+
+                authClaims.AddRange(userRoles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+                var token = new JwtSecurityToken(
+                    issuer: _configuration["Jwt:Issuer"],
+                    expires: DateTime.Now.AddMinutes(double.Parse(_configuration["Jwt:ExpiryMinutes"]!)),
+                    claims: authClaims,
+                    signingCredentials: new Microsoft.IdentityModel.Tokens.SigningCredentials(
+                        new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(
+                            System.Text.Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!)),
+                        Microsoft.IdentityModel.Tokens.SecurityAlgorithms.HmacSha256)
+                    );
+
+                return Ok(new { Token = new JwtSecurityTokenHandler().WriteToken(token) });
+            }
+            return Unauthorized("Invalid username or password!");
+        }
+
+        //add role
+        [HttpPost("add-role")]
+        public async Task<IActionResult> AddRole([FromBody] UserRole model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if (user == null)
+                return NotFound("User not found!");
+            if (!await _roleManager.RoleExistsAsync(model.Role))
+                return NotFound("Role not found!");
+            var result = await _userManager.AddToRoleAsync(user, model.Role);
+            if (result.Succeeded)
+                return Ok("Role added to user successfully!");
+            return BadRequest(result.Errors);
+
+        }
+
+        [HttpPost("assign-role")]
+        public async Task<IActionResult> AssignRole([FromBody] UserRole model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Username);
+            if (user == null)
+                return NotFound("User not found!");
+            if (!await _roleManager.RoleExistsAsync(model.Role))
+                return NotFound("Role not found!");
+            var result = await _userManager.AddToRoleAsync(user, model.Role);
+            if (result.Succeeded)
+                return Ok("Role assigned to user successfully!");
+            return BadRequest(result.Errors);
+        }
+    }
+}
