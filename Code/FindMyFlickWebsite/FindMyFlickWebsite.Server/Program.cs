fa5d7b5941fde7b3f9f17ec1a6d659ea10274176
Caitlin Hemmert <47947419+cait-404@users.noreply.github.com>
2026-03-23 15:55:54 -0400

    Update CORS policy to allow hosted frontend origin

--- Code/FindMyFlickWebsite/FindMyFlickWebsite.Server/Program.cs
+++ Code/FindMyFlickWebsite/FindMyFlickWebsite.Server/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
         policy =>
         {
             policy
-                .WithOrigins("http://localhost:5173")
+                .WithOrigins("https://thankful-mud-03220200f.4.azurestaticapps.net")
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
