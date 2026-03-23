7f98d46273d6f4c325fbf328170e5e80bd0a42e9
Caitlin Hemmert <47947419+cait-404@users.noreply.github.com>
2026-03-23 14:47:55 -0400

    Update API URL to use environment variable
    Update fetch call to variable

--- src/context/MovieContext.jsx
+++ src/context/MovieContext.jsx
@@ -8,7 +8,7 @@ export function MovieProvider({ children }) {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("https://localhost:5002/api/Movies")
+    fetch('${import.meta.env.REACT_API_URL}/api/Movies')
       .then((res) => {
         if (!res.ok) throw new Error("Failed to fetch movies");
         return res.json();
