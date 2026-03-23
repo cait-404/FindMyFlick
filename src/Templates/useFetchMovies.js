d429533bf7a1f00b9e08b1606ada984deb7b5676
Caitlin Hemmert <47947419+cait-404@users.noreply.github.com>
2026-03-23 15:11:49 -0400

    Update fetch URL to use environment variable

--- src/Templates/useFetchMovies.js
+++ src/Templates/useFetchMovies.js
@@ -10,7 +10,7 @@ export default function useFetchMovies(endpoint) {
     if (!endpoint) return;
 
     setLoading(true);
-    fetch(`http://localhost:5135/api/${endpoint}`)
+    fetch(`${import.meta.env.REACT_API_URL}/api/${endpoint}`)
       .then((res) => {
         if (!res.ok) throw new Error("Failed to fetch data");
         return res.json();
