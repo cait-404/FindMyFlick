70458d51581abe04c6ae25237421b4b642a01150
cait-404 <47947419+cait-404@users.noreply.github.com>
2026-03-26 11:46:19 -0400

    Troubleshooting1

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const API_URL = "https://findmyflick-api-ecargjhzf4acffec.canadacentral-01.azurewebsites.net";
+const API_URL = import.meta.env.VITE_API_URL;
 
-export default API_URL
+export default API_URL;
