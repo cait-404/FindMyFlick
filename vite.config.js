b591b470f87c341ca825db181c6e1a58c2b08add
Caitlin Hemmert <47947419+cait-404@users.noreply.github.com>
2026-03-28 09:55:45 -0400

    Add VITE_API_URL to Vite configuration
    Troubleshooting

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [ tailwindcss(), react()],
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify('https://findmyflick-api-ecargjhzf4acffec.canadacentral-01.azurewebsites.net')
+  }
 })
